a18113e4860b64ea9f71719f7bb3dd22bf3d38e2
Bleuzen <supgesu@gmail.com>
2018-05-09 20:54:42 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TMP_DIR=$(mktemp --directory)
+
+cd $TMP_DIR
+wget https://github.com/Bleuzen/manjaro-kde-setup/archive/master.tar.gz
+tar -xzvf master.tar.gz
+cd manjaro-kde-setup-master/bleuzen-manjaro-kde-setup
+makepkg -si
